c7908362e7c29a9c9304984620cf705abcb6852c
gerson <gerson.freire@gmail.com>
2024-11-05 18:51:29 -0300

    Add CNPJ validation and formatting in CNPJController

--- src/python/api/controllers/cnpj_controller.py
+++ src/python/api/controllers/cnpj_controller.py
@@ -1,5 +1,13 @@
 from dao.cnpj_dao import CNPJDao
 
+import sys,os
+
+# adicione o caminho da pasta raiz do script ao path do python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from dv import DigitoVerificador
+import sys
+import os
+
 class CNPJController:
     @staticmethod
     def validar_cnpj(cnpj):
@@ -16,6 +24,16 @@ class CNPJController:
     def gerar_cnpj():
         try:
             response = CNPJDao.gerar_cnpj()
+            cnpj = response['cnpj']
+            dv1 = DigitoVerificador(cnpj[:12])
+            dv1char = '{}'.format(dv1.calcula())
+
+            dv2 = DigitoVerificador(cnpj[:12] + dv1char)
+            dv2char = '{}'.format(dv2.calcula())
+
+            dv = f"{dv1char}{dv2char}"
+            response['cnpj'] = cnpj + dv
+            response['cnpj_formatado'] = f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{dv}"
             return response
         except Exception as e:
             raise e
